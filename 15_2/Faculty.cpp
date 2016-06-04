51d0d12e8dafb95cecfc8908bc3510b473dcbf58
J.P. Brastad <jobras@microsoft.com>
2016-06-04 09:06:37 -0700

    Added Faculty definition.

---
+++ 15_2/Faculty.cpp
@@ -0,0 +1,15 @@
+#include "Faculty.h"
+#include <iostream>
+
+Faculty::Faculty()
+{
+	cout << "Enter office hours (i): ";
+	cin >> this->officeHours;
+	cout << "Enter rank (i): ";
+	cin >> this->rank;
+}
+
+string Faculty::toString() const
+{
+	return "Faculty: " + name;
+}

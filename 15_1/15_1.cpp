5186f417d7eccc5ea83d4a9f41188cc9b0e13d8b
J.P. Brastad <jobras@microsoft.com>
2016-06-03 21:15:33 -0700

    Added Triangle implementation.

--- 15_1/15_1.cpp
+++ 15_1/15_1.cpp
@@ -2,10 +2,12 @@
 //
 
 #include "stdafx.h"
-
+#include "Triangle.h"
 
 int main()
 {
+	Triangle t(3, 4, 5);
+	double tArea = t.getArea();
     return 0;
 }
 
---
+++ 15_1/Triangle.cpp
@@ -0,0 +1,43 @@
+#include "Triangle.h"
+
+Triangle::Triangle()
+{
+	side1 = 1.0;
+	side2 = 1.0;
+	side3 = 1.0;
+}
+
+Triangle::Triangle(double side1, double side2, double side3)
+{
+	this->side1 = side1;
+	this->side2 = side2;
+	this->side3 = side3;
+}
+
+double Triangle::getSide1() const
+{
+	return side1;
+}
+
+double Triangle::getSide2() const
+{
+	return side2;
+}
+
+double Triangle::getSide3() const
+{
+	return side3;
+}
+
+double Triangle::getArea() const
+{
+	//Area=SQRT(s(s-a)(s-b)(s-c)),
+	//where s=(a+b+c)/2 or perimeter/2
+	double s = this->getPerimeter() / 2.0;
+	return sqrt(s*(s-side1)*(s-side2)*(s-side3));
+}
+
+double Triangle::getPerimeter() const
+{
+	return side1 + side2 + side3;
+}

d16e95eebdddb013a0551ced27b9a286b23d93c2
J.P. Brastad <jobras@microsoft.com>
2016-06-03 21:42:19 -0700

    Added test driver.

--- 15_1/15_1.cpp
+++ 15_1/15_1.cpp
@@ -10,6 +10,8 @@ using namespace std;
 int main()
 {
 	double s1, s2, s3;
+	string color;
+	bool filled;
 	cout << "Enter three sides of a triangle:";
 	cin >> s1 >> s2 >> s3;
 	if (!isValidSides(s1, s2, s3))
@@ -17,6 +19,20 @@ int main()
 		cout << "Those are not valid sides for an actual triangle. Goodbye." << endl;
 		return 1;
 	}
+	cout << "Enter color: ";
+	cin >> color;
+	cout << "Color filled: yes 1, no 0: ";
+	cin >> filled;
+	
+	Triangle t(s1, s2, s3);
+	t.setColor(color);
+	t.setFilled(filled);
+	
+	cout << "Area: " << t.getArea() << endl;
+	cout << "Perimeter: " << t.getPerimeter() << endl;
+	cout << "Color: " << t.getColor() << endl;
+	string fill = t.isFilled() ? "true" : "false";
+	cout << "Filled: " << fill << endl;
 
     return 0;
 }

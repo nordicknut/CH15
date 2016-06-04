2387b3ec2cf17dbcbc445baf4bf119e85f271832
jpb <jpbrastad@gmail.com>
2016-06-04 16:21:21 -0700

    Updated. Prints out account type and balances.

--- 15_4/15_4.cpp
+++ 15_4/15_4.cpp
@@ -9,19 +9,19 @@ using namespace std;
 
 int main()
 { 
-	Account* a1 = new Account();
+	Account a1;
 	Account a2(1000.0);
-	CheckingAccount* c1 = new CheckingAccount();
+	CheckingAccount c1;
 	CheckingAccount c2(2000.0);
-	SavingsAccount* s1 = new SavingsAccount();
+	SavingsAccount s1;
 	SavingsAccount s2(3000.0);
  
 
-	cout << (*a1).toString() << endl;
+	cout << a1.toString() << endl;
 	cout << a2.toString() << endl;
-	cout << (*c1).toString() << endl;
+	cout << c1.toString() << endl;
 	cout << c2.toString() << endl;
-	cout << (*s1).toString() << endl;
+	cout << s1.toString() << endl;
 	cout << s2.toString() << endl;
 
     return 0;

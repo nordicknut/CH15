7340555ae706bf1c50ce11219beb682e9c8ae5c3
J.P. Brastad <jobras@microsoft.com>
2016-06-04 00:09:43 -0700

    Added test driver function. Doesn't build, missing externals (not yet implemented).

--- 15_2/15_2.cpp
+++ 15_2/15_2.cpp
@@ -2,6 +2,13 @@
 //
 
 #include "stdafx.h"
+#include "Faculty.h"
+#include "Staff.h"
+#include "Student.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
 //			Person
 //		/			\
 //	Employee		Student
@@ -10,6 +17,19 @@
 
 int main()
 {
+	vector<Person> v;
+	v.push_back(Person());
+	v.push_back(Employee());
+	v.push_back(Faculty());
+	v.push_back(Staff());
+	v.push_back(Student());
+
+	for (size_t i = 0; i < v.size(); i++)
+	{
+		string temp = v.at(i).toString();
+		cout << temp;
+	}
+
     return 0;
 }
 

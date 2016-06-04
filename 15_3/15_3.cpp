dd2e6d0d1178bffb945d27969d3f663412fa6c8a
J.P. Brastad <jobras@microsoft.com>
2016-06-04 11:01:05 -0700

    Updated test driver.

--- 15_3/15_3.cpp
+++ 15_3/15_3.cpp
@@ -9,9 +9,11 @@ int main()
 {
 	MyPoint a(-2, 1);
 	MyPoint b(1, 5);
-	double test = a.getDistance(b);//s/b 5;
+	double test1 = a.getDistance(b);//s/b 5;
 
-	
+	ThreeDPoint c(9, 2, 7);
+	ThreeDPoint d(4, 8, 10);
+	double test2 = c.getDistance(d);//s/b ~8.37
 
     return 0;
 }

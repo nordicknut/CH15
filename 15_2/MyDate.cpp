86f6d7052cd12e08395d06c9ac5f8405883aa014
J.P. Brastad <jobras@microsoft.com>
2016-06-04 09:16:49 -0700

    Added MyDate definitions. Updated Student definition.
    Builds successfully with these changes.

---
+++ 15_2/MyDate.cpp
@@ -0,0 +1,15 @@
+#include "MyDate.h"
+
+MyDate::MyDate()
+{
+	this->year = 2016;
+	this->month = 1;
+	this->year = 1;
+}
+
+MyDate::MyDate(int year, int month, int day)
+{
+	this->year = year;
+	this->month = month;
+	this->day = day;
+}
